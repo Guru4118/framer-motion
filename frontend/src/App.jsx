0e9eb384fa3975021b9bdac863cbd5938ddb398d
Guru4118 <gprasath103@gmail.com>
2025-06-10 01:03:37 +0530

    completed all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import V1l from './V1/v1.jsx';
 import Viedo2 from './V1/V2.jsx'; // default/home
-import Review from './V1/Review.jsx';
-import Quick from './V1/Quick.jsx';
-import Footer from './V1/QuickLink.jsx';
+
 
 function App() {
   return (
@@ -14,10 +11,6 @@ function App() {
         {/* Default Route */}
         <Route path="/" element={<Viedo2 />} />
 
-        {/* Additional Routes */}
-        
-        <Route path="/review" element={<Review />} />
-        <Route path="/footer" element={<Footer />} />
       </Routes>
     </Router>
   );
